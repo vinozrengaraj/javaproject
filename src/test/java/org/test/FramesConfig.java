6565ef9f2d489c3dee88346a5b8b59d22a46a640
vinozrengaraj <vinozr@gmail.com>
2021-07-02 15:31:37 +0530

    Test

---
+++ src/test/java/org/test/FramesConfig.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class FramesConfig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		WebDriverManager.Chromedriver.setup();
+
+	}
+
+}
